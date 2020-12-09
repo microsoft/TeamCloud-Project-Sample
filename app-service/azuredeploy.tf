98062d7dc202ef41960d5315457bc44dc5fbf1f4
Markus Heiliger <markus.heiliger@microsoft.com>
2020-12-08 20:46:02 -0800

    ...

--- app-service/azuredeploy.tf
+++ app-service/azuredeploy.tf
@@ -40,14 +40,4 @@ resource "azurerm_app_service" "test" {
     dotnet_framework_version = "v4.0"
     scm_type                 = "LocalGit"
   }
-
-  app_settings {
-    SOME_KEY = "some-value"
-  }
-
-  connection_string {
-    name  = "Database"
-    type  = "SQLServer"
-    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
-  }
 }
