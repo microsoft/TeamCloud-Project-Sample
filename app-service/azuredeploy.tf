c918dbbdde55a86f41751168e2968843864a9b2d
Markus Heiliger <markus.heiliger@microsoft.com>
2020-12-08 19:48:13 -0800

    ...

--- app-service/azuredeploy.tf
+++ app-service/azuredeploy.tf
@@ -7,17 +7,17 @@ resource "random_id" "server" {
 }
 
 variable "EnvironmentResourceGroupName" {
-  type = "string"
+  type = string
 }
  
 data "azurerm_resource_group" "test" {
-  name     = "${var.EnvironmentResourceGroupName}"
+  name     = var.EnvironmentResourceGroupName
 }
 
 resource "azurerm_app_service_plan" "test" {
   name                = "some-app-service-plan"
-  location            = "${data.azurerm_resource_group.test.location}"
-  resource_group_name = "${data.azurerm_resource_group.test.name}"
+  location            = data.azurerm_resource_group.test.location
+  resource_group_name = data.azurerm_resource_group.test.name
 
   sku {
     tier = "Standard"
@@ -26,10 +26,10 @@ resource "azurerm_app_service_plan" "test" {
 }
 
 resource "azurerm_app_service" "test" {
-  name                = "${random_id.server.hex}"
-  location            = "${data.azurerm_resource_group.test.location}"
-  resource_group_name = "${data.azurerm_resource_group.test.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.test.id}"
+  name                = random_id.server.hex
+  location            = data.azurerm_resource_group.test.location
+  resource_group_name = data.azurerm_resource_group.test.name
+  app_service_plan_id = azurerm_app_service_plan.test.id
 
   site_config {
     dotnet_framework_version = "v4.0"
