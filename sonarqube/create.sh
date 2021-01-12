34685f1d7d5be1aa795a5387985617eaa2742c97
Markus Heiliger <markus.heiliger@microsoft.com>
2021-01-12 11:31:30 -0800

    ...

--- sonarqube/create.sh
+++ sonarqube/create.sh
@@ -68,7 +68,7 @@ echo "- Installing plugin: sonar-auth-aad-plugin-1.1.jar"
 [ "$( az storage file exists --subscription $ComponentSubscription --account-name "$SQACCNAME" --account-key "$SQACCKEY" --share-name "extensions" --path "plugins/sonar-auth-aad-plugin-1.1.jar" --query "exists" -o tsv)" == "false" ] && {  
     curl -s "https://github.com/hkamel/sonar-auth-aad/releases/download/1.1/sonar-auth-aad-plugin-1.1.jar" --output "/var/tmp/sonar-auth-aad-plugin-1.1.jar" 
     az storage directory create --subscription $ComponentSubscription --account-name "$SQACCNAME" --account-key "$SQACCKEY" --share-name "extensions" --name "plugins" -o none
-    az storage file upload --subscription $ComponentSubscription --account-name "$SQACCNAME" --account-key "$SQACCKEY" --share-name "extensions" --path "plugins/sonar-auth-aad-plugin-1.1.jar" --source "/var/tmp/sonar-auth-aad-plugin-1.1.jar" -o none
+    az storage file upload      --subscription $ComponentSubscription --account-name "$SQACCNAME" --account-key "$SQACCKEY" --share-name "extensions" --path "plugins/sonar-auth-aad-plugin-1.1.jar" --source "/var/tmp/sonar-auth-aad-plugin-1.1.jar" --no-progress -o none
 }
 
 echo "- Configuring plugin: sonar-auth-aad-plugin-1.1.jar"
