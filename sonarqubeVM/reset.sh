7aeabe35030f11d03a0bf0546e9b5dac99de4c7e
Markus Heiliger <markus.heiliger@microsoft.com>
2021-01-13 14:38:44 -0800

    ...

--- sonarqubeVM/reset.sh
+++ sonarqubeVM/reset.sh
@@ -11,11 +11,12 @@ error() {
 # get the default runner script path to execute first before adding custom stuff
 SCRIPT="$(find /docker-runner.d -maxdepth 1 -iname "$(basename "$0")")"
 
+trace "Deleting script extensions"
 if [ ! -z "$ComponentResourceGroup" ]; then
 	echo "$(az vm list --subscription $ComponentSubscription -g $ComponentResourceGroup --query "[].name" -o tsv)" | while read VMNAME; do
 
-		IDS=$(az vm extension list --subscription $ComponentSubscription -g $ComponentResourceGroup --vm-name $VMNAME --query '[?typePropertiesType == 'CustomScript'].id' -o tsv)
-		[ ! -z "$IDS" ] && az vm extension delete --ids ${IDS} # delete all custom script extensions so we can rerun out deployment template with failing because of conflicts
+		IDS=$(az vm extension list --subscription $ComponentSubscription -g $ComponentResourceGroup --vm-name $VMNAME --query '[?typePropertiesType == 'CustomScript'].id' -o tsv)		
+		[ ! -z "$IDS" ] && echo "$IDS" && az vm extension delete --ids ${IDS} # delete all custom script extensions so we can rerun out deployment template with failing because of conflicts
 
 	done
 fi
