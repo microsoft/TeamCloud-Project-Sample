3166374c0de2c352f479c037006fc21a155a4de7
Markus Heiliger <markus.heiliger@microsoft.com>
2021-01-12 16:16:17 -0800

    ...

--- sonarqube/create.sh
+++ sonarqube/create.sh
@@ -13,14 +13,15 @@ waitFor() {
         while read data; do waitFor $data; done;
     else
         local RETRYMAX=60
-        local RETRY=0
+        local RETRYWEB=0
+        local RETRYAPI=0
 
-        ((RETRY=0)) && echo -n "Web ($1): ." && until [ $(curl -o /dev/null --silent --fail https://$1) ]; do
-             ((RETRY=RETRY+1)) && [ "$RETRY" >= "$RETRYMAX" ] && ( echo " timeout after $RETRY retries" && exit 1 ) || ( echo -n '.' && sleep 5 )
+        echo -n "Web ($1): ." && until [ $(curl -o /dev/null --silent --fail https://$1) ]; do
+             ((RETRYWEB=RETRYWEB+1)) && [ "$RETRYWEB" >= "$RETRYMAX" ] && ( echo " timeout after $RETRYWEB retries" && exit 1 ) || ( echo -n '.' && sleep 5 )
         done && echo ' done'
 
-        ((RETRY=0)) && echo -n "API ($1): ." && until [ "$(curl -s https://$1/api/system/status | jq --raw-output '.status')" == "UP" ]; do
-            ((RETRY=RETRY+1)) && [ "$RETRY" >= "$RETRYMAX" ] && ( echo " timeout after $RETRY retries" && exit 1 ) || ( echo -n '.' && sleep 5 )
+        echo -n "API ($1): ." && until [ "$(curl -s https://$1/api/system/status | jq --raw-output '.status')" == "UP" ]; do
+            ((RETRYAPI=RETRYAPI+1)) && [ "$RETRYAPI" >= "$RETRYMAX" ] && ( echo " timeout after $RETRYAPI retries" && exit 1 ) || ( echo -n '.' && sleep 5 )
         done && echo ' done'
     fi
 }
