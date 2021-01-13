6324cd46c62931cf1fbaab978bacafd6a6024536
Markus Heiliger <markus.heiliger@microsoft.com>
2021-01-13 15:11:22 -0800

    ....

--- sonarqubeVM/reset.sh
+++ sonarqubeVM/reset.sh
@@ -16,7 +16,7 @@ if [ ! -z "$ComponentResourceGroup" ]; then
 	echo "$(az vm list --subscription $ComponentSubscription -g $ComponentResourceGroup --query "[].name" -o tsv)" | while read VMNAME; do
 
 		az vm extension list --subscription $ComponentSubscription -g $ComponentResourceGroup --vm-name $VMNAME
-		IDS=$(az vm extension list --subscription $ComponentSubscription -g $ComponentResourceGroup --vm-name $VMNAME --query '[?typePropertiesType == "CustomScript"].id' -o tsv)		
+		IDS="$(az vm extension list --subscription $ComponentSubscription -g $ComponentResourceGroup --vm-name $VMNAME --query "[?typePropertiesType == 'CustomScript'].id" -o tsv)"
 
 		echo "- VM: $VMNAME ($IDS)"
 		[ ! -z "$IDS" ] && az vm extension delete --ids ${IDS} # delete all custom script extensions so we can rerun out deployment template with failing because of conflicts
