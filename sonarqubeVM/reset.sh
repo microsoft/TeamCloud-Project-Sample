fc971cc09daa6bb98fae522494c29a0fd6247b9d
Markus Heiliger <markus.heiliger@microsoft.com>
2021-01-13 16:13:19 -0800

    ...

--- sonarqubeVM/reset.sh
+++ sonarqubeVM/reset.sh
@@ -8,25 +8,26 @@ error() {
     echo "Error: $@" 1>&2
 }
 
+deleteCustomScriptExtensions() {
+	local ComponentResourceGroup="$1" 
+	echo "$(az vm list --subscription $ComponentSubscription -g $ComponentResourceGroup --query "[].name" -o tsv)" | while read VMNAME; do
+		IDS="$(az vm extension list --subscription $ComponentSubscription -g $ComponentResourceGroup --vm-name $VMNAME --query "[?typePropertiesType == 'CustomScript'].id" -o tsv)"
+		[ ! -z "$IDS" ] && echo "- $IDS" && az vm extension delete --ids ${IDS} # delete all resolved custom script extensions by their ID
+	done && echo ""
+}
+
 # get the default runner script path to execute first before adding custom stuff
 SCRIPT="$(find /docker-runner.d -maxdepth 1 -iname "$(basename "$0")")"
 
 trace "Deleting script extensions"
-if [ ! -z "$ComponentResourceGroup" ]; then
-	echo "$(az vm list --subscription $ComponentSubscription -g $ComponentResourceGroup --query "[].name" -o tsv)" | while read VMNAME; do
-
-		az vm extension list --subscription $ComponentSubscription -g $ComponentResourceGroup --vm-name "$VMNAME"
-		az vm extension list --subscription $ComponentSubscription -g $ComponentResourceGroup --vm-name $VMNAME --query "[?typePropertiesType == 'CustomScript'].id"
-
-		IDS="$(az vm extension list --subscription $ComponentSubscription -g $ComponentResourceGroup --vm-name $VMNAME --query "[?typePropertiesType == 'CustomScript'].id" -o tsv)"
-
-		az vm extension list --subscription $ComponentSubscription -g $ComponentResourceGroup --vm-name $VMNAME --query "[?typePropertiesType == 'CustomScript'].id"
-
-		echo "- VM: $VMNAME ($IDS)"
-		[ ! -z "$IDS" ] && echo "- $IDS" && az vm extension delete --ids ${IDS} # delete all custom script extensions so we can rerun out deployment template with failing because of conflicts
-
-	done
+if [ -z "$ComponentResourceGroup" ]; then
+    echo "$(az group list --query "[].name" -o tsv)" | while read rg; do
+        deleteCustomScriptExtensions "$rg"
+    done
+else
+    deleteCustomScriptExtensions "$ComponentResourceGroup"
 fi
+
 
 # isolate task script execution in sub shell  
 ( exec "$SCRIPT"; exit $? ) || exit $?
