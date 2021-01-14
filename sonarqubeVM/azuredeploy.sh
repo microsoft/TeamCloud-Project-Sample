4d92775002f45a0c1b03e3d970806a86968eecea
Markus Heiliger <markus.heiliger@microsoft.com>
2021-01-14 11:58:51 -0800

    ...

--- sonarqubeVM/azuredeploy.sh
+++ sonarqubeVM/azuredeploy.sh
@@ -15,21 +15,20 @@ PARAM_CONNECTIONSTRING="$( echo "${3}" | base64 --decode )"
 ARCHITECTURE="x64"
 ARCHITECTURE_BIT="64"
 
-trace() {
-    echo -e "\n>>> $(date '+%F %T'): $@\n"
-}
-
-# trace "Fetching OIC metadata"
 # OIC_AUTHORIZATION_ENDPOINT=$(curl -s "https://login.microsoftonline.com/$PARAM_OIC_TENANT_ID/v2.0/.well-known/openid-configuration" | jq --raw-output '.authorization_endpoint')
 # OIC_TOKEN_ENDPOINT=$(curl -s "https://login.microsoftonline.com/$PARAM_OIC_TENANT_ID/v2.0/.well-known/openid-configuration" | jq --raw-output '.token_endpoint')
 
-trace "Fetching VM metadata"
 VM_NAME=$(curl -s -H Metadata:true "http://169.254.169.254/metadata/instance/compute/name?api-version=2017-08-01&format=text")
 VM_LOCATION=$(curl -s -H Metadata:true "http://169.254.169.254/metadata/instance/compute/location?api-version=2017-08-01&format=text")
 VM_FQN=$(echo $(curl -s -H Metadata:true "http://169.254.169.254/metadata/instance/compute/tags?api-version=2017-08-01&format=text") | grep -Po 'ServiceUrl:(?:(?!;).)*' | grep -Po '(?<=:\/\/).*')
 
+trace() {
+    echo -e "\n>>> $(date '+%F %T'): $@\n"
+}
+
 trace "Updating hosts file"
 sed -i "s/127.0.0.1 localhost/127.0.0.1 localhost $(cat /etc/hostname) $VM_FQN/g" /etc/hosts
+cat /etc/hosts
 
 trace "Updating & upgrading packages"
 apt-get update && apt-get upgrade -y
@@ -40,12 +39,12 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 
 trace "Installing NGINX & CertBot"
 ACCEPT_EULA=Y apt-get install -y nginx 
-snap install --classic certbot && \
-	ln -s /snap/bin/certbot /usr/bin/certbot && \
-	certbot --nginx --register-unsafely-without-email --agree-tos -d $VM_FQN
+snap install --classic certbot 
+[ ! -L /usr/bin/certbot ] && ln -s /snap/bin/certbot /usr/bin/certbot
+certbot --nginx --register-unsafely-without-email --agree-tos -d $VM_FQN
 
 trace "Initialize data disk"
-[ -d "/datadrive" ] || {
+[ -d "/datadrive" ] && cat /etc/fstab || {
 	printf "n\np\n1\n\n\nw\n" | fdisk /dev/sdc
 	mkfs -t ext4 /dev/sdc1
 	mkdir /datadrive
@@ -53,28 +52,30 @@ trace "Initialize data disk"
 	tee -a /etc/fstab << END 
 UUID=$(blkid /dev/sdc1 -s UUID -o value)   /datadrive   ext4   defaults,nofail   1   2
 END
-}
+} 
 
-trace "Create data disk folders"
+trace "Initialize data folders"
 [ -d "/datadrive/data" ] || mkdir /datadrive/data 
 [ -d "/datadrive/temp" ] || mkdir /datadrive/temp
+ls /datadrive
 
-# SONARQUBE_DB_USERNAME="sonar"
-# SONARQUBE_DB_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+SONARUSERNAME="sonar"
+SONARPASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
 # trace "Preparing SonarQube database"
 # SQLCMD_HOME=/opt/mssql-tools/bin
-# $SQLCMD_HOME/sqlcmd -S tcp:$PARAM_DATABASE_SERVER.database.windows.net,1433 -d master -U $PARAM_ADMINUSERNAME -P $PARAM_ADMINPASSWORD -Q "CREATE LOGIN $SONARQUBE_DB_USERNAME WITH PASSWORD='$SONARQUBE_DB_PASSWORD';"
-# $SQLCMD_HOME/sqlcmd -S tcp:$PARAM_DATABASE_SERVER.database.windows.net,1433 -d $PARAM_DATABASE_NAME -U $PARAM_ADMINUSERNAME -P $PARAM_ADMINPASSWORD -Q "CREATE USER $SONARQUBE_DB_USERNAME FROM LOGIN $SONARQUBE_DB_USERNAME; exec sp_addrolemember 'db_owner', '$SONARQUBE_DB_USERNAME';"
+# $SQLCMD_HOME/sqlcmd -S tcp:$PARAM_DATABASE_SERVER.database.windows.net,1433 -d master -U $PARAM_ADMINUSERNAME -P $PARAM_ADMINPASSWORD -Q "CREATE LOGIN $SONARUSERNAME WITH PASSWORD='$SONARPASSWORD';"
+# $SQLCMD_HOME/sqlcmd -S tcp:$PARAM_DATABASE_SERVER.database.windows.net,1433 -d $PARAM_DATABASE_NAME -U $PARAM_ADMINUSERNAME -P $PARAM_ADMINPASSWORD -Q "CREATE USER $SONARUSERNAME FROM LOGIN $SONARUSERNAME; exec sp_addrolemember 'db_owner', '$SONARUSERNAME';"
 
 # configure SonarQube run-as user
 trace "Configuring SonarQube (run-as user)"
-groupadd sonar
-useradd -c "Sonar System User" -d /opt/sonarqube -g sonar -s /bin/bash sonar
+[ ! $(getent group $SONARUSERNAME) ] && groupadd $SONARUSERNAME
+[ ! `id -u $SONARUSERNAME 2>/dev/null || echo -1` -ge 0 ] && useradd -c "Sonar System User" -d /opt/sonarqube -g $SONARUSERNAME -s /bin/bash $SONARUSERNAME
 chown -R sonar:sonar /opt/sonarqube
 chown sonar:sonar /datadrive/data
 chown sonar:sonar /datadrive/temp
-sed -i s/\#RUN_AS_USER=/RUN_AS_USER=sonar/g /opt/sonarqube/bin/linux-x86-$ARCHITECTURE_BIT/sonar.sh
+sed -i s/\#RUN_AS_USER=/RUN_AS_USER=$SONARUSERNAME/g /opt/sonarqube/bin/linux-x86-$ARCHITECTURE_BIT/sonar.sh 
+cat /opt/sonarqube/bin/linux-x86-$ARCHITECTURE_BIT/sonar.sh 
 
 # configure SonarQube settings
 trace "Configuring SonarQube (properties)"
