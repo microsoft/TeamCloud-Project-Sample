f426390d182ae64b84e0532ac07460f6d7797fd6
Markus Heiliger <markus.heiliger@microsoft.com>
2021-01-14 20:50:49 -0800

    ...

--- sonarqubeVM/azuredeploy.sh
+++ sonarqubeVM/azuredeploy.sh
@@ -24,7 +24,6 @@ readonly SONARUSERNAME="sonar"
 readonly SONARPASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 readonly SONARVERSION="7.9.5"
 
-
 trace() {
     echo -e "\n>>> $(date '+%F %T'): $@\n"
 }
@@ -45,6 +44,22 @@ getVMFQN() {
 	curl -s -H Metadata:true "http://169.254.169.254/metadata/instance/compute?api-version=2020-10-01" | jq --raw-output '"\(.name).\(.location).cloudapp.azure.com"'
 }
 
+# =========================================================================================================
+# Updating host system
+# The system need some core settings changed to enable SonarQube (ElasticSearch)
+# =========================================================================================================
+
+trace "Updating host system"
+sysctl -w vm.max_map_count=262144 > /dev/null
+sysctl --system
+
+# =========================================================================================================
+# Installing prerequisites and core services
+# - Register some custom package feeds
+# - Update & upgrade current packages
+# - Install utilities, NGINX, and SonarQube
+# =========================================================================================================
+
 trace "Registering package feeds"
 curl -s https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl -s https://packages.microsoft.com/config/ubuntu/18.04/prod.list >> /etc/apt/sources.list.d/msprod.list
@@ -54,23 +69,12 @@ apt-get update && apt-get upgrade -y
 snap install core && snap refresh core
 
 trace "Installing Utilities"
-ACCEPT_EULA=Y apt-get install -y jq unzip  
-
-trace "Updating hosts file"
-sed -i "s/127.0.0.1 localhost/127.0.0.1 localhost $(cat /etc/hostname) $( getVMFQN )/g" /etc/hosts
-cat /etc/hosts
-
-trace "Installing Azure CLI"
-curl -sL https://aka.ms/InstallAzureCLIDeb | bash
-
-trace "Installing MSSQL Tools"
-ACCEPT_EULA=Y apt-get install -y mssql-tools unixodbc-dev
+ACCEPT_EULA=Y apt-get install -y jq unzip mssql-tools unixodbc-dev
 
 trace "Installing NGINX & CertBot"
 ACCEPT_EULA=Y apt-get install -y default-jre nginx 
 snap install --classic certbot 
 [ ! -L /usr/bin/certbot ] && ln -s /snap/bin/certbot /usr/bin/certbot
-echo "- Creating SSL certificate for $( getVMFQN )"
 certbot --nginx --register-unsafely-without-email --agree-tos --noninteractive -d "$( getVMFQN )"
 
 trace "Installing SonarQube"
@@ -83,6 +87,65 @@ trace "Installing SonarQube"
 	unzip $SONARARCHIVE && mv ./sonarqube-$SONARVERSION /opt/sonarqube
 }
 
+trace "Updating host names"
+sed -i "s/127.0.0.1 localhost/127.0.0.1 localhost $(cat /etc/hostname) $( getVMFQN )/g" /etc/hosts
+cat /etc/hosts
+
+
+# =========================================================================================================
+# Configuring SonarQube (web)
+# - Configure NGINX to forward traffic to SonarQube
+# - Restart NGINX to activate configuration
+# =========================================================================================================
+
+trace "Configuring SonarQube (web)"
+tee /etc/nginx/sites-available/default << END
+upstream sonarqube {
+    server 127.0.0.1:9000 fail_timeout=0;
+}
+
+server {
+    if (\$host = $( getVMFQN )) {
+        return 301 https://\$host\$request_uri;
+    } # managed by Certbot
+
+    listen 80 ;
+    listen [::]:80 ;
+
+    server_name $( getVMFQN );
+    return 404; # managed by Certbot
+}
+
+server {
+    listen [::]:443 ssl ipv6only=on; # managed by Certbot
+    listen 443 ssl; # managed by Certbot
+
+    server_name $( getVMFQN );
+
+    ssl_certificate /etc/letsencrypt/live/$( getVMFQN )/fullchain.pem; # managed by Certbot
+    ssl_certificate_key /etc/letsencrypt/live/$( getVMFQN )/privkey.pem; # managed by Certbot
+
+    include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+
+    location / {
+        proxy_set_header        Host \$host;
+        proxy_set_header        X-Real-IP \$remote_addr;
+        proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header        X-Forwarded-Proto \$scheme;
+        proxy_redirect          http://$( getVMFQN ) https://$( getVMFQN );
+        proxy_pass              http://sonarqube;
+    }
+}
+END
+nginx -s reload
+
+# =========================================================================================================
+# Configuring SonarQube (user)
+# - Create user & group
+# - Grant ownership permissions and set RunAsUser
+# =========================================================================================================
+
 trace "Configuring SonarQube (user)"
 [ ! $(getent group $SONARUSERNAME) ] && { 
 	echo "- Creating group: $SONARUSERNAME"
@@ -92,12 +155,20 @@ trace "Configuring SonarQube (user)"
 	echo "- Creating user: $SONARUSERNAME" 
 	useradd -c "Sonar System User" -d /opt/sonarqube -g $SONARUSERNAME -s /bin/bash $SONARUSERNAME 
 }
+
 echo "- Granting $SONARUSERNAME SonarQube ownership"
 chown -R sonar:sonar /opt/sonarqube
+
 echo "- Setting $SONARUSERNAME as RunAsUser"
 sed -i s/\#RUN_AS_USER=/RUN_AS_USER=$SONARUSERNAME/g /opt/sonarqube/bin/linux-x86-$ARCHITECTURE_BIT/sonar.sh 
 
-trace "Configuring SonarQube (data disk)"
+# =========================================================================================================
+# Configuring SonarQube (storage)
+# - Mount storage drive and initialize volumne
+# - Create folder structure and grant access
+# =========================================================================================================
+
+trace "Configuring SonarQube (storage)"
 [ -d "/datadrive" ] && cat /etc/fstab || {
 	printf "n\np\n1\n\n\nw\n" | fdisk /dev/sdc
 	mkfs -t ext4 /dev/sdc1
@@ -107,16 +178,24 @@ trace "Configuring SonarQube (data disk)"
 UUID=$(blkid /dev/sdc1 -s UUID -o value)   /datadrive   ext4   defaults,nofail   1   2
 END
 } 
-
-trace "Configuring SonarQube (data directories)"
 mkdir -p /datadrive/data && chown sonar:sonar /datadrive/data
 mkdir -p /datadrive/temp && chown sonar:sonar /datadrive/temp
 find /datadrive -maxdepth 1 -group sonar
+
+# =========================================================================================================
+# Configuring SonarQube (database)
+# - Grant user database ownership
+# =========================================================================================================
 
 trace "Configuring SonarQube (database)"
 readonly SQLCMD_HOME=/opt/mssql-tools/bin
 $SQLCMD_HOME/sqlcmd -S tcp:$PARAM_DATABASESERVER,1433 -d master -U $PARAM_ADMINUSERNAME -P $PARAM_ADMINPASSWORD -Q "CREATE LOGIN $SONARUSERNAME WITH PASSWORD='$SONARPASSWORD';" >/dev/null
 $SQLCMD_HOME/sqlcmd -S tcp:$PARAM_DATABASESERVER,1433 -d $PARAM_DATABASENAME -U $PARAM_ADMINUSERNAME -P $PARAM_ADMINPASSWORD -Q "CREATE USER $SONARUSERNAME FROM LOGIN $SONARUSERNAME; exec sp_addrolemember 'db_owner', '$SONARUSERNAME';" >/dev/null
+
+# =========================================================================================================
+# Configuring SonarQube (properties)
+# - Update SonarQube configuration (database & storage)
+# =========================================================================================================
 
 trace "Configuring SonarQube (properties)"
 [ -z "$(grep 'CUSTOM CONFIGURATION' /opt/sonarqube/conf/sonar.properties)" ] && tee -a /opt/sonarqube/conf/sonar.properties << END
@@ -133,7 +212,11 @@ sonar.path.temp=/datadrive/temp
 
 END
 
-# configure SonarQube as a service
+# =========================================================================================================
+# Configuring SonarQube (service)
+# - Configure systemd for SonarQube
+# =========================================================================================================
+
 trace "Configuring SonarQube (service)"
 tee /etc/systemd/system/sonar.service << END
 [Unit]
@@ -154,15 +237,22 @@ Restart=always
 WantedBy=multi-user.target
 END
 
-# initalize SonarQube database
-trace "Intializing SonarQube database"
-/opt/sonarqube/bin/linux-x86-$ARCHITECTURE_BIT/sonar.sh console &
-timeout=$(($(date +%s)+300)) # now plus 5 minutes
-while [ "$timeout" -ge "$(date +%s)" ]; do
-	status="$( curl -s http://localhost:9000/api/system/status | jq --raw-output '.status' )"
-	[ "$status" == "UP" ] && break || { echo "- $status"; sleep 5; }
-done
+# =========================================================================================================
+# Intializing SonarQube
+# - Force database initialization by starting SonarQube in console mode
+# - Enable SonarQube to run as service
+# =========================================================================================================
 
-# enable SonarQube service
-trace "Enabling SonarQube service"
+trace "Intializing SonarQube"
+
+echo "- Starting SonarQube Console"
+/opt/sonarqube/bin/linux-x86-$ARCHITECTURE_BIT/sonar.sh console &
+
+timeout=$(($(date +%s)+300)) # now plus 5 minutes
+echo "  ."; while [ "$timeout" -ge "$(date +%s)" ]; do
+	status="$( curl -s http://localhost:9000/api/system/status | jq --raw-output '.status' )"
+	[ "$status" == "UP" ] && { echo ". done"; break; } || { echo -n "."; sleep 5; }
+done;
+
+echo "- Enabling SonarQube Service"
 systemctl enable sonar
