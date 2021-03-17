c89325808e15e9254887f2606ab77018c2b68dea
Markus Heiliger <markus.heiliger@microsoft.com>
2021-03-16 22:38:57 -0700

    ...

--- app-service/main.tf
+++ app-service/main.tf
@@ -15,17 +15,15 @@ provider "azurerm" {
   skip_provider_registration = true
 }
 
-# resource "azurerm_resource_group" "main" {
-#   name     = var.resourceGroupName
-#   location = var.resourceGroupLocation
-# }
+resource "azurerm_resource_group" "main" {
+  name     = var.resourceGroupName
+  location = var.resourceGroupLocation
+}
 
 resource "azurerm_app_service_plan" "main" {
   name                = "${sha1(azurerm_resource_group.main.id)}-plan"
-  location            = var.resourceGroupLocation
-  resource_group_name = var.resourceGroupName
-  # location            = azurerm_resource_group.main.location
-  # resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 
   sku {
     tier = "Basic"
@@ -35,10 +33,8 @@ resource "azurerm_app_service_plan" "main" {
 
 resource "azurerm_app_service" "main" {
   name                = "${sha1(azurerm_resource_group.main.id)}-website"
-  location            = var.resourceGroupLocation
-  resource_group_name = var.resourceGroupName
-  # location            = azurerm_resource_group.main.location
-  # resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
   app_service_plan_id = azurerm_app_service_plan.main.id
 
   site_config {
