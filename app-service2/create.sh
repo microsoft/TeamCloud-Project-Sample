4201711a4e31149dcbd3f9c185e6ac95c7a22082
Markus Heiliger <markus.heiliger@microsoft.com>
2021-03-17 16:50:15 -0700

    ...

---
+++ app-service2/create.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+trace() {
+    echo -e "\n>>> $@ ...\n"
+}
+
+echo "============================================================"
+echo "="
+echo "= CREATE"
+echo "="
+echo "============================================================"
+
+readonly ComponentState="/mnt/storage/component.tfstate"
+readonly ComponentPlan="/mnt/storage/component.tfplan"
+
+rm -f $ComponentPlan # delete any existing plan file
+
+trace "Terraform Info"
+terraform -version
+
+trace "Initializing Terraform"
+terraform init -no-color
+
+if [[ (! -z "$ComponentResourceGroup") && (! -f "$ComponentState") ]]; then
+	ComponentResourceGroupId="$(az group show -n $ComponentResourceGroup --query id -o tsv)"
+	ComponentResourceGroupLocation="$(az group show -n $ComponentResourceGroup --query location -o tsv)"
+	while read terraformFile; do
+		trace "Initializing Terraform ResourceGroups ($terraformFile)"
+		while read tfrg; do
+
+			echo -e "\n- Importing $ComponentResourceGroupId into $tfrg\n"
+			terraform import -no-color -lock=true -state=$ComponentState -var "resourceGroupName=$ComponentResourceGroup" -var "resourceGroupLocation=$ComponentResourceGroupLocation" $tfrg $ComponentResourceGroupId
+
+		done < <(cat $terraformFile | hcl2json | jq --raw-output '.resource.azurerm_resource_group // empty | to_entries [] | "azurerm_resource_group.\(.key)"')
+		echo "- done."
+	done < <(find . -maxdepth 1 -type f -name '*.tf')
+fi
+
+trace "Updating Terraform Plan"
+terraform plan -no-color -refresh=true -lock=true -state=$ComponentState -out=$ComponentPlan -var "resourceGroupName=$ComponentResourceGroup" -var "resourceGroupLocation=$(az group show -n $ComponentResourceGroup --query location -o tsv)"
+
+trace "Applying Terraform Plan"
+terraform apply -no-color -auto-approve -lock=true -state=$ComponentState $ComponentPlan
+
+# tail -f /dev/null
---
+++ app-service2/readme.md
@@ -0,0 +1,9 @@
+# App Service
+
+This template deploys an App Service plan and an App Service app on Linux. It's compatible with all supported programming languages on App Service.
+
+![](https://azurecomcdn.azureedge.net/cvt-d49e1b7812fb156a3d74b5723a95043aa707b7e51cfc9e4697dd420349f4008a/images/page/services/functions/value-prop-5.svg)
+
+---
+
+[Learn more about Azure App Service](https://azure.microsoft.com/en-us/services/app-service/)
