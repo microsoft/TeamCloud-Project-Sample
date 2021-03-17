34e653926a7778e17bd47add93cae839b4c4ae44
Markus Heiliger <markus.heiliger@microsoft.com>
2021-03-16 20:51:24 -0700

    ...

--- app-service/main.tf
+++ app-service/main.tf
@@ -4,8 +4,8 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      source = "hashicorp/azurerm"
+      version = "2.51.0"
     }
   }
 }
