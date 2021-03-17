889076ed2ae117d53923c9e3e2e5e471e8992d00
Markus Heiliger <markus.heiliger@microsoft.com>
2021-03-16 20:28:56 -0700

    ...

--- app-service/delete.sh
+++ app-service/delete.sh
@@ -22,9 +22,7 @@ trace "Initializing Terraform"
 terraform init -no-color
 
 trace "Updating Terraform Plan"
-terraform plan -no-color -refresh=true -lock=true -destroy -state=$ComponentStateFile -out=$ComponentTemplatePlan \ 
-    -var "resourceGroupName=$ComponentResourceGroup" \
-    -var "resourceGroupLocation=$(az group show -n $ComponentResourceGroup --query location -o tsv)"
+terraform plan -no-color -refresh=true -lock=true -destroy -state=$ComponentStateFile -out=$ComponentTemplatePlan -var "resourceGroupName=$ComponentResourceGroup" -var "resourceGroupLocation=$(az group show -n $ComponentResourceGroup --query location -o tsv)"
 
 trace "Applying Terraform Plan"
 terraform apply -no-color -auto-approve -lock=true -state=$ComponentStateFile $ComponentTemplatePlan
