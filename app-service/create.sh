bd2875c4cf1a34dbb18cdb7580b379f5f70ce709
Markus Heiliger <markus.heiliger@microsoft.com>
2021-03-17 10:26:03 -0700

    ...

--- app-service/create.sh
+++ app-service/create.sh
@@ -24,6 +24,20 @@ terraform -version
 trace "Initializing Terraform"
 terraform init -no-color
 
+if [ ! -z "$ComponentResourceGroup" && ! -f "$ComponentStateFile" ]; then
+	ComponentResourceGroupId=$(az group show -n $ComponentResourceGroup --query id -o tsv)
+	while read terraformFile; do
+		trace "Initializing Terraform ResourceGroups ($terraformFile)"
+		while read rg; do
+
+			echo "- Importing $ComponentResourceGroupId into $rg"
+			terraform import -no-color -lock=true -state=$ComponentStateFile $rg $ComponentResourceGroupId -var "resourceGroupName=$ComponentResourceGroup" -var "resourceGroupLocation=$(az group show -n $ComponentResourceGroup --query location -o tsv)"
+
+		done < <(cat $terraformFile | hcl2json | jq --raw-output '.resource.azurerm_resource_group | to_entries [] | "azurerm_resource_group.\(.key)"')
+		echo "- done."
+	done < <(find . -maxdepth 1 -type f -name '*.tf')
+fi
+
 trace "Updating Terraform Plan"
 terraform plan -no-color -refresh=true -lock=true -state=$ComponentStateFile -out=$ComponentTemplatePlan -var "resourceGroupName=$ComponentResourceGroup" -var "resourceGroupLocation=$(az group show -n $ComponentResourceGroup --query location -o tsv)"
 
