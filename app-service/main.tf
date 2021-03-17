7a73602493aa87e027af16749538c34eb47b81ea
Markus Heiliger <markus.heiliger@microsoft.com>
2021-03-16 20:44:49 -0700

    ...

--- app-service/main.tf
+++ app-service/main.tf
@@ -1,3 +1,14 @@
+# We strongly recommend using the required_providers block 
+# to set the Azure Provider source and version being used
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
 provider "azurerm" {
   features {}
 }
