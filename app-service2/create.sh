adc16f45c5870b86f0e6191c153c75f3af963c21
Markus Heiliger <markus.heiliger@microsoft.com>
2021-03-17 19:43:15 -0700

    ...

--- app-service2/create.sh
+++ app-service2/create.sh
@@ -5,6 +5,9 @@ trace() {
 }
 
 readonly ComponentState="/mnt/storage/component.tfstate"
+readonly ComponentPlan="/mnt/storage/component.tfplan"
+
+rm -f $ComponentPlan # reset terraform plan to start fresh
 
 trace "Terraform Info"
 terraform -version
@@ -12,7 +15,10 @@ terraform -version
 trace "Initializing Terraform"
 terraform init -no-color
 
+trace "Creating Terraform Plan"
+terraform plan -no-color -refresh=true -lock=true -state=$ComponentState -out=$ComponentPlan -var "resourceGroupName=$ComponentResourceGroup"
+
 trace "Applying Terraform Plan"
-terraform apply -no-color -refresh=true -auto-approve -lock=true -state=$ComponentState -var "resourceGroupName=$ComponentResourceGroup"
+terraform apply -no-color -auto-approve -lock=true -state=$ComponentState $ComponentPlan
 
 # tail -f /dev/null
