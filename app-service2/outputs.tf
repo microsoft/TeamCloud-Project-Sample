b61322cd2355ac4ea4af20a0a8d80236689fffc3
Markus Heiliger <markus.heiliger@microsoft.com>
2021-03-17 17:49:27 -0700

    ...

--- app-service2/outputs.tf
+++ app-service2/outputs.tf
@@ -1,7 +1,7 @@
 output "app_service_name" {
-  value = azurerm_app_service.main.name
+  value = azurerm_app_service.component.name
 }
 
 output "app_service_default_hostname" {
-  value = "https://${azurerm_app_service.main.default_site_hostname}"
+  value = "https://${azurerm_app_service.component.default_site_hostname}"
 }
--- app-service2/variables.tf
+++ app-service2/variables.tf
@@ -2,6 +2,3 @@ variable "resourceGroupName" {
   description = "Name of the target resource group"
 }
 
-variable "resourceGroupLocation" {
-  description = "Location of the target resource group"
-}
